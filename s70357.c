6be711096ef486329c00b993026e817834a69f8f
Markus Klemm <markus@markusklemm.net>
2016-05-24 21:15:38 +0200

    EVP_Encryption finished

--- s70357.c
+++ s70357.c
@@ -5,13 +5,64 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <openssl/evp.h>
 
-int main (int argc, char* argv[]){
-    if (argc != 1+ 3){
-        printf("Usage %s",argv[0]);
+
+/*!
+ * @param cipher_text Buffer, at least (plain_len + cipher_block_size - 1) bytes big,
+ * where the encrypted data will be stored.
+ * @param cipher_text_len Actual length of encrypted data in cipher_text in bytes
+ */
+bool mk_evp_encrypt(const uint8_t *plain_text,
+                    const int plain_len,
+                    uint8_t *cipher_text,
+                    int *cipher_text_len,
+                    const EVP_CIPHER *cipher,
+                    unsigned char *key,
+                    unsigned char *iv) {
+    EVP_CIPHER_CTX *context = EVP_CIPHER_CTX_new();
+    if (!context) {
+        return false;
+    }
+    if (!EVP_EncryptInit_ex(context, cipher, NULL, key, iv)) {
+        EVP_CIPHER_CTX_free(context);
+        return false;
+    }
+
+    *cipher_text_len = 0;
+    if (!EVP_EncryptUpdate(context, cipher_text, cipher_text_len, plain_text, plain_len)) {
+        EVP_CIPHER_CTX_free(context);
+        return false;
+    }
+    if (!EVP_EncryptFinal_ex(context, cipher_text + *cipher_text_len, cipher_text_len)) {
+        EVP_CIPHER_CTX_free(context);
+        return false;
+    }
+
+    EVP_CIPHER_CTX_free(context);
+    return true;
+}
+
+void decrypt(FILE *cipher_text, FILE plain_text, FILE *key, int corrupt_byte_pos, int corrupt_byte_count) {
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage %s -<MODE>\n", argv[0]);
+        printf("\t<MODE>: d Decrypt aka Aufgabe 1\n");
         return EXIT_FAILURE;
     }
+    switch (*++argv[1]) {
+        case 'd':
+
+            break;
+        default:
+            break;
+    }
+    //EVP_des_ede_cbc
 
     return EXIT_SUCCESS;
 }
