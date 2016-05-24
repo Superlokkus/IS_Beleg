ca9dc60c63a48ea652684660b3a003a1dd3c1c30
Markus Klemm <markus@markusklemm.net>
2016-05-24 14:11:36 +0200

    Fixed openssl cmake

--- s70357.c
+++ s70357.c
@@ -2,6 +2,16 @@
  * IS_Beleg by Markus Klemm
  * */
 
-int main (int argc, char* argv[]){
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <openssl/evp.h>
 
+int main (int argc, char* argv[]){
+    if (argc != 1+ 3){
+        printf("Usage %s",argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
